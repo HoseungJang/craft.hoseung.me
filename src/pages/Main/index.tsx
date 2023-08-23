846e75efb5dc678ec6cae0cc99227abd6ffc6897
HoseungJang <hsjang.dev@gmail.com>
2023-08-24 05:45:20 +0900

    fix main

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -95,6 +95,7 @@ export function Main() {
         className={css`
           width: 100%;
 
+          margin-bottom: 4rem;
           padding: 16px;
         `}
       >
@@ -145,14 +146,28 @@ function Row({ children }: { children: ReactNode }) {
       className={css`
         width: 100%;
 
-        background-color: rgba(0, 0, 0, 0.1);
+        background-color: rgba(255, 255, 255, 0.1);
 
-        border: 2px solid rgba(0, 0, 0, 0.1);
+        box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
 
         border-radius: 16px;
 
+        transform: scale(1);
+
         &:not(:last-child) {
           margin-bottom: 16px;
+        }
+
+        @media (hover: hover) {
+          transition: background-color 0.4s, box-shadow 0.4s, transform 0.4s;
+
+          &:hover {
+            background-color: rgba(255, 255, 255, 0.15);
+
+            box-shadow: rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;
+
+            transform: scale(1.02);
+          }
         }
       `}
     >
